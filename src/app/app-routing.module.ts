744ed78aafe585965b915c702d77a3be3d226916
vivekanandpv <vivekanandpv@outlook.com>
2021-08-29 12:53:21 +0530

    forms module 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   {
     path: 'forms',
     loadChildren: () => import('./forms/forms.module')
-      .then(m => m.FormsModule)
+      .then(m => m.AngularFormsModule)
   },
   {
     path: '**',
--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FormsRoutingModule } from './forms-routing.module';
 import { TFormComponent } from './t-form/t-form.component';
 import { RFormComponent } from './r-form/r-form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { RFormComponent } from './r-form/r-form.component';
   ],
   imports: [
     CommonModule,
-    FormsRoutingModule
+    FormsRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
-export class FormsModule { }
+export class AngularFormsModule { }
